6f6973f2c46e6e4ef4dedd8096f2fbeaee7d69ba
OmegaCrack <47146118+OmegaCrack@users.noreply.github.com>
2020-08-31 20:13:16 +0300

    Updated Post.js

--- models/Post.js
+++ models/Post.js
@@ -35,7 +35,8 @@ const PostSchema = new Schema({
         required: true
       },
       name: {
-        type: String
+        type: String,
+        required: true
       },
       avatar: {
         type: String
